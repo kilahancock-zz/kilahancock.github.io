472e43a3d3d2c18feac32166a19dfe18dc021ef3
Kila Hancock <kilahancock@gmail.com>
2020-08-02 13:22:26 -0400

    Update Experience.vue

--- components/Experience.vue
+++ components/Experience.vue
@@ -2,9 +2,11 @@
     <div id="ex">
         <h1>Red Ventures &#183; Charlotte, NC &#183; Summer 2020</h1>
         <p><i>A portfolio of digital companies that brings consumers and
-            brands<br>together through powerful, customized online experiences.</i></p><br>
+          brands<br>together through powerful, customized online experiences.</i></p><br>
         <h3>Front End Web Developer</h3>
-        <p>Javascript, Vue.js, Nuxt.js, React.js</p>
+        <p>Working with Vue.js and Nuxt.js frameworks to contribute to websites<br>
+          for partners in the TeleCom vertical.
+        </p>
         <br><br><br>
         <h1>Daylight Data &#183; Chapel Hill, NC Jan &#183; '19 - Jan '20</h1>
         <p><i>Provides contract software development services for early-stage ventures.</i></p><br>
